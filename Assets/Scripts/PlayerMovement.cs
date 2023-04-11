13bef46064167a1690729cd1db1151d1d69ea18b
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-04-11 19:03:25 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public CharacterController controller;
+
+	public float runSpeed = 40f;
+
+	public float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+	public Animator animator;
+
+    void Update()
+	{
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		if (Input.GetKeyDown("w"))
+		{
+			jump = true;
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			crouch = true;
+		}
+		else if (Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			crouch = false;
+		}
+
+		Cursor.lockState = CursorLockMode.Confined;
+
+	}
+
+	void FixedUpdate()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
+}
