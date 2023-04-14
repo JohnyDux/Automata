5d7957adb8265a8b55edc957d8f4fca5149fc195
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-04-14 03:44:32 +0100

    Font Update

--- Assets/Scripts/HammerController.cs
+++ Assets/Scripts/HammerController.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class HammerController : MonoBehaviour
 {
     Animator anim;
+    public Rigidbody2D controller;
+    public int HitIntensity;
     void Start()
     {
-        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>().m_Rigidbody2D;
     }
     void Update()
     {
-        StartCoroutine(Wait(anim));
+        StartCoroutine(Wait(anim, controller));
     }
 
-    IEnumerator Wait(Animator anim)
+    IEnumerator Wait(Animator anim, Rigidbody2D controller)
     {
         anim.SetBool("Hit", true);
-        yield return new WaitForSecondsRealtime(2);
+        controller.AddForce(Vector2.up*HitIntensity);
+        yield return new WaitForSeconds(2);
         anim.SetBool("Hit", false);
     }
 }
